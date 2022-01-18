59958e2cdf323772c924b124e56e1c77d32bfad1
lanyi <lanyi@ra3.moe>
2022-01-19 00:59:05 +0100

    remove old project as it can be built now

--- ModulePchConcept/dllmain.cpp
+++
@@ -1,9 +0,0 @@
-// dllmain.cpp : Defines the entry point for the DLL application.
-#include "pch.h"
-import Module;
-
-__declspec(dllexport) std::size_t baka()
-{
-    Lanyi::Test::Baka auto f = [](std::string_view a, std::string_view b) { return a; };
-    return f("Cirno", "9").size();
-}
